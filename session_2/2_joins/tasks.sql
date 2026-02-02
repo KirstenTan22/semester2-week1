2179951f90269795445c55f70519805d807f47ba
Kirsten Robyn Santos Tan <sbld0152@leeds.ac.uk>
2026-02-02 12:43:42 +0000

    Still working on session 2 task 3

--- session_2/2_joins/tasks.sql
+++ session_2/2_joins/tasks.sql
@@ -8,7 +8,7 @@
 -- 3. Exit SQLite: .exit
 
 -- 1. List all posts that have been deleted.
-SELECT * FROM posts WHERE deleted = true;
+SELECT * FROM posts WHERE deleted = 1;
 
 -- 2. List all users whose usernames begin with 'a'.
 SELECT * FROM users WHERE username LIKE 'a%'; 
@@ -17,19 +17,23 @@ SELECT * FROM users WHERE username LIKE 'a%';
 SELECT * FROM posts JOIN users ON user_id = users.id WHERE content LIKE '%small%';
 
 -- 4. Return each non-deleted post and the username of the person who wrote in, in date order.
-SELECT *, users.username FROM posts JOIN users ON posts.user_id = users.id WHERE deleted = false ORDER BY created_on ASC;
+SELECT posts.*, users.username FROM posts JOIN users ON posts.user_id = users.id WHERE deleted = 0 ORDER BY created_on ASC;
 
 -- 5. For each user, return their username and the total number of posts they have made.
-SELECT users.username, COUNT(*) as TotalPosts FROM posts JOIN users on posts.user_id = users.id GROUP BY users.id, users.username;
+SELECT users.username, COUNT(posts.*) as TotalPosts FROM users LEFT JOIN posts on posts.user_id = users.id GROUP BY users.id, users.username;
 
 -- 6. Modify the previous query to return only users who have authored more than 3 posts.
-SELECT users.username, COUNT(*) as TotalPosts FROM posts JOIN users on posts.user_id = users.id GROUP BY users.id, users.username HAVING COUNT(*) > 3;
+SELECT users.username, COUNT(posts.*) as TotalPosts FROM users JOIN posts on posts.user_id = users.id GROUP BY users.id, users.username;
+ HAVING COUNT(*) > 3;
 
 -- 7. For each user, return their username and the number of users they follow, including users who follow no one.
-SELECT U.username, COUNT(F.following_user_id) AS TotalFollowed FROM users U LEFT JOIN follows F ON u.id = F.following_user_id GROUP BY u.id, u.username;
+SELECT U.username, COUNT(F.followed_user_id) AS TotalFollowed FROM users U LEFT JOIN follows F ON u.id = F.following_user_id GROUP BY u.id, u.username;
 
 -- 8. List the top 5 most-followed users.
-SELECT U.username, COUNT(F.followed_user_id) as TotalFollowers FROM users U JOIN follows F on U.id = F.followed_user_id GROUP BY U.id, U.username ORDER BY TotalFollowers DESC LIMIT 5;
+SELECT U.username, COUNT(F.followed_user_id) as TotalFollowers FROM users U JOIN follows F ON U.id = F.followed_user_id GROUP BY U.id, U.username ORDER BY TotalFollowers DESC LIMIT 5;
 
 -- 9. Return a list showing the username of the follower and the username of the followed user for each follow relationship.
+SELECT U1.username as Follower, U2.username Followed FROM follows F JOIN users U1 ON U1.id = F.following_user_id JOIN users U2 ON U2.id = F.followed_user_id;
+
 -- 10. For a given user (christopherprice), return all non-deleted posts written by users they follow, ordered from most recent to oldest.
+SELECT * FROM users as U JOIN follows F ON U.id = F.following_user_id JOIN posts P ON F.followed_user_id = P.user_id WHERE U.username = 'christopherprice' AND P.deleted = 0 ORDER BY P.created_on DESC;
--- session_2/3_practice/tasks.sql
+++ session_2/3_practice/tasks.sql
@@ -7,4 +7,93 @@
 -- 2. Load this script: .read tasks.sql
 -- 3. Exit SQLite: .exit
 
+-- 1. List all tracks along with their album title and artist name.
+SELECT Track.*, Album.Title, Artist.Name FROM Track JOIN Album ON Track.AlbumId = Album.AlbumId JOIN Artist ON Album.ArtistId = Artist.ArtistId;
 
+-- 2. List all albums along with the name of the artist.
+SELECT Album.*, Artist.Name FROM Album JOIN Artist on Album.ArtistId = Artist.ArtistId;
+
+-- 3. For each genre, list the number of tracks available.
+SELECT Genre.Name, Count(*) FROM Track JOIN Genre ON Track.GenreId = Genre.GenreId GROUP BY Track.GenreId;
+
+-- 4. List the total duration (milliseconds) of tracks per genre.
+SELECT Genre.Name, SUM(milliseconds) FROM Track JOIN Genre ON Track.GenreId = Genre.GenreId GROUP BY Track.GenreId;
+
+-- 5. For each media type, count the number of tracks.
+SELECT MediaType.Name, Count(*) FROM Track JOIN MediaType ON Track.MediaTypeId = MediaType.MediaTypeId GROUP BY MediaType.MediaTypeId;
+
+-- 6. Find the total number of tracks per album.
+SELECT Album.Title, Count(*) FROM Track JOIN Album ON Track.AlbumId = Album.AlbumId GROUP BY Track.AlbumId;
+
+-- 7. Find the average price of tracks per genre.
+SELECT Genre.Name, AVG(Track.UnitPrice) FROM Track JOIN Genre ON Track.GenreId = Genre.GenreId GROUP BY Track.GenreId;
+
+-- 8. List all tracks along with genre, album, and artist name.
+SELECT Track.*, Genre.Name, Album.Title, Artist.Name FROM Track 
+JOIN Genre ON Track.GenreId = Genre.GenreID 
+JOIN Album ON Track.AlbumId = Album.AlbumId
+JOIN Artist ON Album.ArtistId = Artist.ArtistId;
+
+-- 9. Find the top 5 albums with the most tracks.
+SELECT Album.Title, COUNT(*) FROM Track JOIN Album ON Track.AlbumId = Album.AlbumId GROUP BY Track.AlbumId ORDER BY Count(*) DESC LIMIT 5;
+
+-- 10. Find the top 5 artists with the most albums.
+SELECT Artist.Name, Count(*) FROM Album JOIN Artist ON Album.ArtistId = Artist.ArtistId GROUP BY Album.ArtistId ORDER BY Count(*) DESC LIMIT 5;
+
+-- 11. For each genre, find the total revenue generated from sales.
+SELECT Genre.Name, SUM(InvoiceLine.UnitPrice * InvoiceLine.Quantity) 
+FROM Genre
+    LEFT JOIN Track ON Genre.GenreId = Track.GenreId 
+    LEFT JOIN InvoiceLine ON Track.TrackId = InvoiceLine.TrackId
+GROUP BY Genre.GenreId, Genre.Name;
+
+-- 12. Find the top 5 genres by total revenue.
+SELECT Genre.Name, SUM(InvoiceLine.UnitPrice * InvoiceLine.Quantity) as TotalRevenue 
+FROM Genre
+    LEFT JOIN Track ON Genre.GenreId = Track.GenreId 
+    LEFT JOIN InvoiceLine ON Track.TrackId = InvoiceLine.TrackId
+GROUP BY Genre.GenreId, Genre.Name
+ORDER BY TotalRevenue DESC LIMIT 5;
+
+-- 13. For each artist, calculate the total number of tracks sold and total revenue from sold tracks.
+SELECT Artist.Name, COUNT(InvoiceLine.InvoiceLineId) AS '#TracksSold', SUM(InvoiceLine.UnitPrice * InvoiceLine.Quantity) as TotalRevenue
+FROM Artist 
+    LEFT JOIN Album on Artist.ArtistId = Album.ArtistId 
+    LEFT JOIN Track on Album.AlbumId = Track.AlbumId
+    LEFT JOIN InvoiceLine on Track.TrackId = InvoiceLine.TrackId
+GROUP BY Artist.ArtistId, Artist.Name;
+
+-- 14. List albums with at least one track sold and total revenue from sold tracks.
+SELECT Album.Title, COUNT(InvoiceLine.InvoiceLineId), SUM(InvoiceLine.UnitPrice * InvoiceLine.Quantity)
+FROM Album
+    JOIN Track on Album.AlbumId = Track.AlbumId
+    JOIN InvoiceLine on Track.TrackId = InvoiceLine.TrackId
+GROUP BY Album.AlbumId, Album.Title;
+
+-- 15. Find the top 5 artists by total revenue.
+SELECT Artist.Name, SUM(InvoiceLine.UnitPrice * InvoiceLine.Quantity) as TotalRevenue
+FROM Artist
+    LEFT JOIN Album on Artist.ArtistId = Album.ArtistId
+    LEFT JOIN Track on Album.AlbumId = Track.AlbumId
+    LEFT JOIN InvoiceLine on Track.TrackId = InvoiceLine.TrackId
+GROUP BY Artist.ArtistId, Artist.Name
+ORDER BY TotalRevenue DESC LIMIT 5;
+
+-- 16. For each media type, calculate the total revenue generated from tracks that have been sold.
+SELECT MediaType.Name, SUM(InvoiceLine.UnitPrice * InvoiceLine.Quantity) as TotalRevenue
+FROM MediaType
+    LEFT JOIN Track on MediaType.MediaTypeId = Track.MediaTypeId
+    LEFT JOIN InvoiceLine on Track.TrackId = InvoiceLine.TrackId
+GROUP BY MediaType.MediaTypeId, MediaType.Name;
+
+-- 17. For each genre, find the average track price and total number of tracks.
+SELECT Genre.Name, AVG(Track.UnitPrice), COUNT(Track.TrackId)
+FROM Genre
+    LEFT JOIN Track on Genre.GenreId = Track.GenreId
+GROUP BY Genre.GenreId, Genre.Name;
+
+-- 18. List the 10 tracks sold in the smallest quantities (tracks with at least one sale).
+SELECT Track.Name
+
+-- 19. Find the top 10 tracks by total revenue.
+-- 20. For each artist, find the average track price and total number of tracks sold.
